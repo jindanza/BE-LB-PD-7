00629c43de005bcde797fa6ffe9abc79c1bf26e5
Muhammad Jindan <jindanza7@gmail.com>
2024-09-21 22:15:46 +0700

    vercel

--- config/db.config.js
+++ config/db.config.js
@@ -5,7 +5,7 @@ module.exports = {
     DB: "padi",
     dialect: "postgres",
     pool: {
-        max: 5,
+        max: 10,
         min: 0,
         acquire: 30000,
         idle: 10000
--- models/index.js
+++ models/index.js
@@ -1,24 +1,31 @@
-const dbConfig = require("../config/db.config.js")
+const dbConfig = require("../config/db.config.js");
 
-const Sequelize = require("sequelize")
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorsAliases: false,
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
-    }
-})
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const db = {};
 
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
 db.user = require("./user.model.js")(sequelize, Sequelize);
-db.product = require("./product.model.js")(sequelize, Sequelize)
+db.product = require("./product.model.js")(sequelize, Sequelize);
 
-module.exports = db
+module.exports = db;
